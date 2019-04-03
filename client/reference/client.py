0fab6639d0c032f37592f2285e5f6839ee759572
Eric Hohenstein <e_hohenstein@yahoo.com>
2019-04-03 02:12:15 -0700

    added about 1/2 of reference client functionality

--- client/reference/client.py
+++ client/reference/client.py
@@ -5,12 +5,29 @@ import optparse
 import websocket
 
 class Client(object):
-    def __init__(self, url, username, chat, ws=websocket):
+    def __init__(self, url, username, chat, timeout, ws=websocket):
         self.conn = None
         self.url = url
         self.username = username
         self.chat = chat
         self.ws = ws
+        self.timeout = timeout
+        self.user_id = None
+        self.chat_users = None
+
+        self.server_hello = False
+        self.has_joined = False
+
+        self.messageDispatch = {
+            'protocol_error': self.protocolError,
+            'server_hello': self.serverHello,
+            'chat_state': self.chatState,
+            'joined': self.joined,
+            'chat_message': self.chatMessage,
+            'left': self.left,
+            'ping_reply': self.pingReply,
+            'server_goodbye': self.serverGoodbye
+        }
 
     def run(self):
         self.conn = self.ws.WebSocketApp(url=self.url, on_open=self.onOpen, on_message=self.onMessage, on_error=self.onError, on_close=self.onClose)
@@ -24,14 +41,121 @@ class Client(object):
         client_hello = {"record": "client_hello", "protocol_version": "1.0", "username": self.username}
         self.conn.send(json.dumps(client_hello))
 
-    def onMessage(self, ws, message):
-        pass
+    def onMessage(self, ws, raw_message):
+        message = None
+        try:
+            message = json.loads(raw_message)
+        except json.decoder.JSONDecodeError:
+            print("non-JSON message received from server: {0}".format(raw_message))
+            self.conn.close()
+            return
+        record_type = message.get('record', None)
+        if not record_type:
+            print("message from server missing record type field: {0}".format(message))
+            self.conn.close()
+            return
+        if not self.server_hello and record_type != 'server_hello':
+            print("invalid message sequence from server prior to server_hello: {0}".format(message))
+            self.conn.close()
+            return
+        if self.server_hello and record_type == 'server_hello':
+            print("invalid message sequence from server, received second server_hello: {0}".format(message))
+            self.conn.close()
+            return
+        handler = self.messageDispatch.get(record_type, None)
+        if not handler:
+            print("unrecognized message from server: {0}".format(message))
+            self.conn.close()
+            return
+        handler(message)
 
     def onError(self, ws, error):
-        pass
+        print("\nwebsocket error: {0}".format(error))
 
     def onClose(self, ws):
+        print("\nwebsocket closed")
+
+    def protocolError(self, message):
+        if self.validate({'record': 'protocol_error', 'code': str, 'reason': str}, message):
+            print("protocol_error: {0}".format(message))
+        self.conn.close()
+
+    def serverHello(self, message):
+        if not self.validate({'record': 'server_hello', 'protocol_version': '1.0', 'user_id': str}, message):
+            self.conn.close()
+            return
+        self.user_id = message['user_id']
+        join = {"record": "join_chat", "chat_name": self.chat}
+        self.conn.send(json.dumps(join))
+        self.server_hello = True
+
+    def chatState(self, message):
+        if not self.validate({'record': 'chat_state', 'chat_name': self.chat, 'users': [{'record': 'chat_user', 'user_name': str, 'user_id': str}]}, message):
+            self.conn.close()
+            return
+        if self.chat_users != None:
+            print("second chat_state record received: {0}".format(message))
+            self.conn.close()
+            return
+        self.chat_users = {}
+
+    def joined(self, message):
+        if not self.validate({'record': 'joined', 'chat_name': self.chat, 'user_name': str, 'user_id': str, 'timestamp': str, 'sequence': int}, message):
+            self.conn.close()
+            return
+        if self.chat_users == None:
+            print("received joined message before chat_state message: {0}".format(message))
+            self.conn.close()
+            return
+        if not self.has_joined:
+            if message['user_name'] != self.username:
+                print("received joined for other user before self: {0}".format(message))
+                self.conn.close()
+                return
+            if message['user_id'] != self.user_id:
+                print("received joined for other user before self: {0}".format(message))
+                self.conn.close()
+                return
+            self.has_joined = True
+        chat_message = {'record': 'chat_message', 'chat_name': self.chat, 'mime_type': 'text/plain', 'message': 'I like cats'}
+        self.conn.send(json.dumps(chat_message))
+
+    def chatMessage(self, message):
         pass
+
+    def left(self, message):
+        pass
+
+    def pingReply(self, message):
+        pass
+
+    def serverGoodbye(self, message):
+        pass
+
+    def validate(self, fields, message):
+        if sorted(fields.keys()) != sorted(message.keys()):
+            print('invalid {0} record received from server: {0}'.format(message['record'], message))
+            return False
+        for key in fields:
+            value = message[key]
+            if type(fields[key]) == list:
+                if type(value) != list:
+                    print('field {0} of {1} record is expected to be a list: {2}'.format(key, message['record'], message))
+                    return False
+                for item in value:
+                    if not self.validate(fields[key][0], item):
+                        return False
+            elif type(fields[key]) == type:
+                if type(value) != fields[key]:
+                    print('field {0} of {1} record does not have the expected type: {2}'.format(key, message['record'], message))
+                    return False
+                if type(value) == str and not value:
+                    print('field {0} of {1} record should not be empty: {2}'.format(key, message['record'], message))
+                    return False
+            elif value != fields[key]:
+                print('field {0} of {1} record is expected to have the value {2}: {3}'.format(key, message['record'], fields[key], message))
+                return False
+        return True
 
 def main(args):
     parser = optparse.OptionParser(usage="usage: %prog --server=<server> --port=<port>")
--- client/reference/client_test.py
+++ client/reference/client_test.py
@@ -32,13 +32,18 @@ class FakeWebSocket(object):
     def send(self, data):
         self.sent_frames.append(data)
 
+    def close(self):
+        self.running = False
+
 class ClientTests(unittest.TestCase):
     def setUp(self):
         self.url = 'ws://foobar:4242/some/path'
         self.ws = FakeWebSocket()
         self.username = "user1234"
         self.chat = "chat5678"
-        self.client = client.Client(url=self.url, username=self.username, chat=self.chat, ws=self.ws)
+        self.user_id = "someone"
+        self.now = "2015-11-12T08:15:51Z"
+        self.client = client.Client(url=self.url, username=self.username, chat=self.chat, timeout=None, ws=self.ws)
         self.client.run()
 
     def test_connects_to_server(self):
@@ -58,6 +63,171 @@ class ClientTests(unittest.TestCase):
         client_hello = json.loads(self.ws.sent_frames[0])
         self.assertEqual({"record": "client_hello", "protocol_version": "1.0", "username": self.username}, client_hello)
 
+    def test_closes_on_bad_json_message(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, 'this is not json')
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_bad_record(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, "{}")
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_protocol_error(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "protocol_error", "code": "ERR-123", "reason": "I don't like you very much"}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_invalid_server_hello(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello"}))
+        self.assertFalse(self.ws.running)
+
+    def test_sends_join_on_valid_server_hello(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        join = json.loads(self.ws.sent_frames[1])
+        self.assertEqual({"record": "join_chat", "chat_name": self.chat}, join)
+
+    def test_closes_on_non_server_hello_message_first(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "ping_reply"}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_second_server_hello(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": "someone_else"}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_bad_chat_state_message(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state"}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_wrong_chat_name_in_chat_state_message(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": "other_chat", "users": []}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_second_chat_state_message(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_bad_joined_message(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined"}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_joined_before_chat_state(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": self.chat, "user_name": self.username, "user_id": self.user_id, "timestamp": self.now, "sequence": 42}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_wrong_joined_chat_name(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": "other_chat", "user_name": self.username, "user_id": self.user_id, "timestamp": self.now, "sequence": 42}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_other_user_name_before_self_joined(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": self.chat, "user_name": "other_user", "user_id": self.user_id, "timestamp": self.now, "sequence": 42}))
+        self.assertFalse(self.ws.running)
+
+    def test_closes_on_other_user_name_before_self_joined(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": self.chat, "user_name": self.username, "user_id": "other_user", "timestamp": self.now, "sequence": 42}))
+        self.assertFalse(self.ws.running)
+
+    def test_can_receive_other_user_join_after_self_join(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": self.chat, "user_name": self.username, "user_id": self.user_id, "timestamp": self.now, "sequence": 42}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": self.chat, "user_name": "other_user", "user_id": "other_user_id", "timestamp": self.now, "sequence": 42}))
+        self.assertTrue(self.ws.running)
+
+    def test_sends_chat_message_after_joining(self):
+        self.client.onOpen(self.ws)
+        self.client.onMessage(self.ws, json.dumps({"record": "server_hello", "protocol_version": "1.0", "user_id": self.user_id}))
+        self.client.onMessage(self.ws, json.dumps({"record": "chat_state", "chat_name": self.chat, "users": []}))
+        self.client.onMessage(self.ws, json.dumps({"record": "joined", "chat_name": self.chat, "user_name": self.username, "user_id": self.user_id, "timestamp": self.now, "sequence": 42}))
+        chat_message = json.loads(self.ws.sent_frames[2])
+        self.assertEqual({'record': 'chat_message', 'chat_name': self.chat, 'mime_type': 'text/plain', 'message': 'I like cats'}, chat_message)
+
+    def test_closes_on_bad_chat_message(self):
+        pass
+
+    def test_closes_on_chat_message_from_user_not_in_chat(self):
+        pass
+
+    def test_allows_chat_from_user_in_chat(self):
+        pass
+
+    def test_sends_ping_on_chat_from_self(self):
+        pass
+
+    def test_closes_on_bad_left_message(self):
+        pass
+
+    def test_closes_on_left_message_for_user_not_in_chat(self):
+        pass
+
+    def test_closes_when_self_left(self):
+        pass
+
+    def test_removes_user_on_left(self):
+        pass
+
+    def test_closes_on_message_before_chat_state(self):
+        pass
+
+    def test_closes_on_left_before_chat_state(self):
+        pass
+
+    def test_closes_on_ping_reply_before_chat_state(self):
+        pass
+
+    def test_message_validation_fails_for_extra_fields(self):
+        self.assertFalse(self.client.validate({'record': 'foobar'}, {'record': 'foobar', 'foo': 'bar'}))
+
+    def test_message_validation_fails_for_missing_fields(self):
+        self.assertFalse(self.client.validate({'record': 'foobar', 'foo': str}, {'record': 'foobar'}))
+
+    def test_message_validation_fails_for_wrong_field_type(self):
+        self.assertFalse(self.client.validate({'record': 'foobar', 'foo': str}, {'record': 'foobar', 'foo': 42}))
+
+    def test_message_validation_fails_for_empty_string_type(self):
+        self.assertFalse(self.client.validate({'record': 'foobar', 'foo': str}, {'record': 'foobar', 'foo': ''}))
+
+    def test_message_validation_fails_for_wrong_exact_value(self):
+        self.assertFalse(self.client.validate({'record': 'foobar', 'foo': 'bar'}, {'record': 'foobar', 'foo': 'baz'}))
+
+    def test_message_validation_fails_for_non_list_in_list_field(self):
+        self.assertFalse(self.client.validate({'record': 'foobar', 'foo': [{'record': 'bar'}]}, {'record': 'foobar', 'foo': 'bar'}))
+
+    def test_message_validation_fails_for_bad_list_field(self):
+        self.assertFalse(self.client.validate({'record': 'foobar', 'foo': [{'record': 'bar'}]}, {'record': 'foobar', 'foo': [{'record': 'baz'}]}))
+
+    def test_message_validation_passes_for_valid_message(self):
+        self.assertTrue(self.client.validate({'record': 'foobar', 'foo': str}, {'record': 'foobar', 'foo': 'baz'}))
+
+    def test_message_validation_passes_for_valid_list_field(self):
+        self.assertTrue(self.client.validate({'record': 'foobar', 'foo': [{'record': 'bar'}]}, {'record': 'foobar', 'foo': [{'record': 'bar'}]}))
 
 unittest.main()
 
