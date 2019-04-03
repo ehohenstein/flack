e02399608aa6a3b1311b577001546f24682ba4ff
Eric Hohenstein <e_hohenstein@yahoo.com>
2019-04-02 23:48:52 -0700

    reference client will send client_hello message when connected

--- client/reference/client.py
+++ client/reference/client.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
-from optparse import OptionParser
+import json
+import optparse
 import websocket
 
 class Client(object):
-    def __init__(self, url, ws=websocket):
+    def __init__(self, url, username, chat, ws=websocket):
         self.conn = None
         self.url = url
+        self.username = username
+        self.chat = chat
         self.ws = ws
 
     def run(self):
-        self.conn = self.ws.WebSocketApp(self.url, self.onOpen, self.onMessage, self.onError, self.onClose)
+        self.conn = self.ws.WebSocketApp(url=self.url, on_open=self.onOpen, on_message=self.onMessage, on_error=self.onError, on_close=self.onClose)
         self.conn.run_forever()
 
     def close(self):
@@ -18,7 +21,8 @@ class Client(object):
             self.conn.close()
 
     def onOpen(self, ws):
-        pass
+        client_hello = {"record": "client_hello", "protocol_version": "1.0", "username": self.username}
+        self.conn.send(json.dumps(client_hello))
 
     def onMessage(self, ws, message):
         pass
@@ -30,8 +34,10 @@ class Client(object):
         pass
 
 def main(args):
-    parser = OptionParser(usage="usage: %prog --server=<server> --port=<port>")
+    parser = optparse.OptionParser(usage="usage: %prog --server=<server> --port=<port>")
     parser.add_option("-u", "--url", dest="url", help="url to use to connect to server")
+    parser.add_option("-n", "--name", dest="username", default="hoser", help="username for (fake) authentication")
+    parser.add_option("-c", "--chat", dest="chat", default="foobar", help="chat name")
     options, extra = parser.parse_args(args)
 
     if extra[1:]:
@@ -44,7 +50,7 @@ def main(args):
         parser.print_help()
         return 1
 
-    c = Client(options.url)
+    c = Client(options.url, options.username, options.chat)
     try:
         c.run()
     except KeyboardInterrupt:
--- client/reference/client_test.py
+++ client/reference/client_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import json
 import unittest
 
 import client
@@ -7,14 +8,19 @@ import client
 class FakeWebSocket(object):
     def __init__(self):
         self.url = None
+
         self.onOpen = None
         self.onMessage = None
         self.onError = None
         self.onClose = None
+
         self.running = False
+
+        self.sent_frames = []
 
     def WebSocketApp(self, url, on_open, on_message, on_error, on_close):
         self.url = url
+        self.onOpen = on_open
         self.onMessage = on_message
         self.onError = on_error
         self.onClose = on_close
@@ -23,19 +29,35 @@ class FakeWebSocket(object):
     def run_forever(self):
         self.running = True
 
+    def send(self, data):
+        self.sent_frames.append(data)
+
 class ClientTests(unittest.TestCase):
     def setUp(self):
         self.url = 'ws://foobar:4242/some/path'
         self.ws = FakeWebSocket()
-        self.client = client.Client(url=self.url, ws=self.ws)
-
-    def test_client_connects_to_server(self):
+        self.username = "user1234"
+        self.chat = "chat5678"
+        self.client = client.Client(url=self.url, username=self.username, chat=self.chat, ws=self.ws)
         self.client.run()
+
+    def test_connects_to_server(self):
         self.assertEqual(self.url, self.ws.url)
 
-    def test_client_runs_forever(self):
-        self.client.run()
+    def test_assigns_callbacks(self):
+        self.assertEqual(self.client.onOpen, self.ws.onOpen)
+        self.assertEqual(self.client.onMessage, self.ws.onMessage)
+        self.assertEqual(self.client.onError, self.ws.onError)
+        self.assertEqual(self.client.onClose, self.ws.onClose)
+
+    def test_runs_forever(self):
         self.assertTrue(self.ws.running)
+
+    def test_sends_client_hello_on_open(self):
+        self.client.onOpen(self.ws)
+        client_hello = json.loads(self.ws.sent_frames[0])
+        self.assertEqual({"record": "client_hello", "protocol_version": "1.0", "username": self.username}, client_hello)
+
 
 unittest.main()
 
